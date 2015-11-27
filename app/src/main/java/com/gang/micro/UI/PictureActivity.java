5a984ad297e21e785fcbabf274a4dbb18fdb5ea8
Gonzalo Acosta <acosta.gonzalo@gmail.com>
2015-11-27 16:34:47 -0300

    Analysus activity layout in landscape mode added.

--- app/src/main/java/com/gang/micro/UI/PictureActivity.java
+++ app/src/main/java/com/gang/micro/UI/PictureActivity.java
@@ -2,6 +2,7 @@ package com.gang.micro.UI;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -26,6 +27,13 @@ public class PictureActivity extends Activity {
 
     @OnClick(R.id.discard_button) void discardPicture() {
         //TODO do something before close the activity
+        finish();
+    }
+
+    @OnClick(R.id.save_button) void savePicture () {
+        Intent analysisActivity = new Intent(PictureActivity.this,AnalysisActivity.class);
+        //TODO put something in the intent?
+        startActivity(analysisActivity);
         finish();
     }
 }
