bb43347110458e023c6913017df73f1e055357d0
nicogarcia <nicoalbo90@gmail.com>
2016-02-04 16:04:42 -0300

    Fixed NSD bug with "\032" string

--- app/src/main/java/com/gang/micro/core/nsd/NSDResolveListener.java
+++ app/src/main/java/com/gang/micro/core/nsd/NSDResolveListener.java
@@ -25,6 +25,9 @@ public class NSDResolveListener implements NsdManager.ResolveListener {
     @Override
     public void onServiceResolved(NsdServiceInfo nsdServiceInfo) {
 
+        // Get service name (replace to avoid "\032" string)
+        String serviceName = getServiceName(nsdServiceInfo);
+
         // Get host
         InetAddress host = nsdServiceInfo.getHost();
 
@@ -32,11 +35,20 @@ public class NSDResolveListener implements NsdManager.ResolveListener {
         String address = host.getHostAddress();
 
         // Create microscope instance
-        Microscope microscope = new Microscope(nsdServiceInfo.getServiceName(), address);
+        Microscope microscope = new Microscope(serviceName, address);
 
         Log.d(this.getClass().getName(), "Resolved address = " + address);
 
         // Notify progress to owning async task
         owningAsyncTask.publish(microscope);
+    }
+
+    private String getServiceName(NsdServiceInfo nsdServiceInfo) {
+        String serviceName = nsdServiceInfo.getServiceName();
+
+        int bugIndex = serviceName.indexOf("\\032");
+
+        return serviceName.substring(0, bugIndex);
+
     }
 }
