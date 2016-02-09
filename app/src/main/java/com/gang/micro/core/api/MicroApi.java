39316d1e073e3bd0402bb9c5ef15c736265be707
nicogarcia <nicoalbo90@gmail.com>
2016-02-08 23:52:00 -0300

    Extended micro api timeout

--- app/src/main/java/com/gang/micro/core/api/MicroApi.java
+++ app/src/main/java/com/gang/micro/core/api/MicroApi.java
@@ -3,6 +3,9 @@ package com.gang.micro.core.api;
 import android.content.Context;
 
 import com.gang.micro.core.MicroApplication;
+import com.squareup.okhttp.OkHttpClient;
+
+import java.util.concurrent.TimeUnit;
 
 import retrofit.JacksonConverterFactory;
 import retrofit.Retrofit;
@@ -18,9 +21,14 @@ public class MicroApi {
 
         String baseUrl = app.getProtocol() + "://" + ip + ":" + app.getWebApplicationPort();
 
+        final OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.setReadTimeout(60, TimeUnit.SECONDS);
+        okHttpClient.setConnectTimeout(60, TimeUnit.SECONDS);
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(JacksonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
 
         api = retrofit.create(MicroApiSpecification.class);
